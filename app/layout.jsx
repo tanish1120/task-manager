e744b871d51778f2c2c34c4268690e57ebb165af
tanish1120 <rathoretanish11@gmail.com>
2026-06-08 02:00:57 +0530

    added favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,21 +13,7 @@ export const metadata = {
   description: 'A modern task manager dashboard to stay on top of your work',
   generator: 'v0.app',
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
+    icon: '/favicon.png',
   },
 }
 
